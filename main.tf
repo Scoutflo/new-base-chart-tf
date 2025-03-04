d1e5966fc5f8f49bbbb4aba4a84cac25ebc3d37f
AtharvaBondreScoutflo <115654880+AtharvaBondreScoutflo@users.noreply.github.com>
2025-03-04 20:32:44 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -130,7 +130,27 @@ resource "aws_eks_addon" "coredns" {
   depends_on = [module.eks]
 }
 
+# --- Kubernetes Provider Configuration ---
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+# --- Pass Kubernetes Provider to Storage Class Module ---
 module "storage_class" {
   source = "./modules/kubernetes-storage-class"
+  providers = {
+    kubernetes = kubernetes
+  }
   depends_on = [module.eks]
 }
